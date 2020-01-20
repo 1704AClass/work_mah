f31a1582f828f28b3f329f2ecb85de7f3c2dc836
maaihong <job_mah@163.com>
2020-01-20 19:37:46 +0800

    1.20作业

---
+++ health_mobile/src/main/java/com/itheima/controller/SetmealController.java
@@ -0,0 +1,19 @@
+/**  
+* @Title: SetmealController.java  
+* @Package com.itheima.controller  
+* @Description: TODO(用一句话描述该文件做什么)  
+* @author mah  
+* @date 2020年1月20日  
+* @version V1.0  
+*/  
+package com.itheima.controller;
+
+/**  
+* @ClassName: SetmealController  
+* @Description: TODO(这里用一句话描述这个类的作用)  
+* @author mah  
+* @date 2020年1月20日    
+*/
+public class SetmealController {
+
+}
